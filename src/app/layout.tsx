056ce3b25f8c281da516903668455f9c9a7789a7
Battugs-01 <tugsking2002@gmail.com>
2024-01-03 21:41:04 +0800

    [second com]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,23 @@
+import Header from '@/components/Header';
 import './globals.css';
 import { Inter } from 'next/font/google';
+import siteMetadata from '@/data/siteMetadata';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Medium like',
+  title: siteMetadata.title,
   description: 'Medium like app',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>{children}</body>
-    </html>
+<div className='mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0'>
+      <div className='flex h-screen flex-col justify-between'>
+        <Header />
+        <main className='mb-auto'>{children}</main>
+        {/* <Footer /> */}
+      </div>
+    </div>
   );
 }
